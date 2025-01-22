bfce2f2b0af64348a1169e599e706b1febcfc368
parisfuchsia <liammagat8.gmail.com>
2025-01-22 08:41:26 +0800

    Another adj in frontend

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className = "bg-[#F0ECCF] inset-x-0 relative dark:bg-[#1F1F21] flex flex-col gap-8 items-center justify-center w-[100vw] bottom-0 p-4">
       <main className = "flex gap-4 items-center justify-center ">
-        <h1 className = "text-[#8B5E3C] dark:text-neutral-300 ">© 2025 Parisfuchxiaa </h1>
+        <h1 className = "text-[#8B5E3C] dark:text-neutral-300 ">© 2025 Parisfuchsia </h1>
         <h5 className = "text-[#8B5E3C] dark:text-neutral-300 ">|</h5>
         <section className = "flex items-center gap-2 ">
           <a href = "https://github.com/parisfuchsia"><FaGithub className = "text-[#8B5E3C]  dark:text-neutral-300  size-10" /></a>
